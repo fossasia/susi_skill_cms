8a06099db5d2730cd1aa64be45bb48ebc4908947
saurabhjn76 <saurabhjn76@gmail.com>
2017-06-14 19:32:18 +0530

    initial skelton for sidebar

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$("#accordian h3").click(function(){
+		//slide up all the link lists
+		$("#accordian ul ul").slideUp();
+		//slide down the link list below the h3 clicked - only if its closed
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	})
+})
