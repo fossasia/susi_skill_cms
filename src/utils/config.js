a6b12ec13ca1f4b109104763f606d9331ea6e7d5
Chetan Kaushik <dynamitechetan@gmail.com>
2017-06-17 13:20:06 +0530

    Added Config.js to store App Settings. Added Functions.js with common functions required for app.

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+import logo from '../assets/Images/Min.jpg';
+
+module.exports = {
+  name: 'SUSI Wiki',
+  footerText: 'SUSI Wiki Fossasia ',
+  logoSrc: logo,
+  logoText: 'SUSI Wiki',
+  defaultSelectMenu: 'home',
+  needBread: false,
+  needFooter: false,
+}
