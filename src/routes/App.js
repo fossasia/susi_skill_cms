60fc70914eb1345992b0180ca541cddc4102ffdb
saurabhjn76 <saurabhjn76@gmail.com>
2017-06-19 23:12:00 +0530

    added sidebar

--- src/routes/App.js
+++ src/routes/App.js
@@ -8,23 +8,46 @@ function App ({ children, location, dispatch, loading, app }) {
 
     const headerProps = {
         user,
-
+        siderFold,
+        location,
+        isNavbar,
+        menuPopoverVisible,
+        switchMenuPopover () {
+            dispatch({ type: 'app/switchMenuPopver' })
+        },
+        logout () {
+            dispatch({
+                type: 'app/logout',
+            })
+        },
+        switchSider () {
+            dispatch({ type: 'app/switchSider', payload: location.pathname })
+        }
     }
 
     const siderProps = {
         siderFold,
         location,
         menuOpenKeys,
+        handleClickNavMenu(value){
+            dispatch({
+                type: 'app/changeMenu',
+                payload: value,
+            })
+        }
     }
 
     return (
         <div>
             {
-              <Index
-                  headerProps={headerProps}
-                  siderProps={siderProps}
-
-              />
+                <Index
+                    headerProps={headerProps}
+                    siderProps={siderProps}
+                    siderFold={siderFold}
+                    isNavbar={isNavbar}
+                    children={children}
+                    location={location}
+                />
             }
         </div>
     );
