82882071d89290da1bf1c1f973c4613dd5ad7d30
Chetan Kaushik <dynamitechetan@gmail.com>
2017-07-06 06:40:42 +0530

    Added Versioning Service, Added Body Component, (#62)
    * Added Versioning Service, Added Body Component,
    
    Added Versioning Service, Added Body Component, Improved Sidebar.
    
    * Removed .idea
    
    * added workspace to gitignore

---
+++ src/components/Body/Body.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Body extends React.Component {
+
+    render() {
+        return (
+            <div style={styles.body}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+const styles = {
+    body: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        padding: "80px 30px 30px",
+        width: "100%"
+    }
+};
