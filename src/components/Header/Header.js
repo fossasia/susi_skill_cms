9674ba2fc412e42ec24c0dc003a7ab1af4d12f4c
SAURABH JAIN <saurabhjn76@gmail.com>
2017-07-29 04:37:42 +0530

    added register component (#142)
    * added singup component
    
    * completed sign up component
    
    * updated header.js with signup component
    
    * fix travis

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from "antd/es/button/button";
 import Cookies from 'universal-cookie';
-import {Dialog} from "material-ui";
+import Dialog from 'material-ui/Dialog';
 import Login from "../Auth/Login/Login";
+import SignUp from "../Auth/SignUp/SignUp";
 import colors from "../../Utils/colors";
+import Close from 'material-ui/svg-icons/navigation/close';
 var deleteCookie = function(name) {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
@@ -14,7 +16,8 @@ export default class Header extends React.Component {
         super(props)
         this.state = {
             showLogout: false,
-            open:false
+            open:false,
+            openSignUp:false
         }
     }
 
@@ -24,11 +27,19 @@ export default class Header extends React.Component {
             this.setState({showLogout: false});
         }
         else
-            this.setState({open: false,showLogout:true});
+            this.setState({open: false, openSignUp:false, showLogout:true});
     }
     handleOpen = () => {
-        this.setState({open: true});
-        console.log("asd");
+        this.setState({open: true, openSignUp: false });
+      
+    };
+    handleOpenSignUp = () => {
+        this.setState({openSignUp: true, open: false});
+      
+    };
+
+    handleClose = () => {
+        this.setState({ open: false, openSignUp: false });
     };
 
     logout = () => {
@@ -39,19 +50,29 @@ export default class Header extends React.Component {
     };
 
     render() {
-
+        const closingStyle ={
+            position: 'absolute',
+            zIndex: 1200,
+            fill: '#444',
+            width: '26px',
+            height: '26px',
+            right: '10px',
+            top: '10px',
+            cursor:'pointer'
+        }
         return (
 
             <div style={styles.header}>
                 { !this.state.showLogout ?
                     <div>
-                        <Button style={styles.buttonMargin}>Register</Button>
+                        <Button onClick={this.handleOpenSignUp} style={styles.buttonMargin}>Register</Button>
                         <Button onClick={this.handleOpen} style={styles.buttonMargin}>Login</Button>
                     </div>
                     :
-                            <Button onClick={this.logout} style={styles.buttonMargin}>Logout</Button>
+                    <Button onClick={this.logout} style={styles.buttonMargin}>Logout</Button>
 
                 }
+
                 <Dialog
                     modal={false}
                     open={this.state.open}
@@ -59,6 +80,18 @@ export default class Header extends React.Component {
                     autoScrollBodyContent={true}
                 >
                     <Login {...this.props} />
+                    <Close style={closingStyle}
+                           onTouchTap={this.handleClose} />
+                </Dialog>
+                <Dialog
+                    modal={false}
+                    open={this.state.openSignUp}
+                    onRequestClose={this.handleClose}
+                    autoScrollBodyContent={true}
+                >
+                    <SignUp {...this.props} />
+                    <Close style={closingStyle}
+                           onTouchTap={this.handleClose} />
                 </Dialog>
             </div>
         );
