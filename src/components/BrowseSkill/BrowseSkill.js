c2daeb01c90d38ab3ad41f529411864d8d31985a
saurabhjn76 <saurabhjn76@gmail.com>
2017-08-30 11:46:42 +0530

    feat: added tooltip in create a skill

--- src/components/BrowseSkill/BrowseSkill.js
+++ src/components/BrowseSkill/BrowseSkill.js
@@ -8,6 +8,7 @@ import Add from 'material-ui/svg-icons/content/add';
 import ContentAdd from 'material-ui/svg-icons/navigation/arrow-forward';
 import { Card } from 'material-ui/Card';
 import * as $ from 'jquery';
+import ReactTooltip from 'react-tooltip';
 import Link from 'react-router-dom/es/Link';
 import colors from '../../Utils/colors';
 import CircleImage from '../CircleImage/CircleImage';
@@ -367,10 +368,11 @@ export default class BrowseSkill extends React.Component {
                                     <ContentAdd />
                                 </FloatingActionButton>
                                 <Link to='/skillCreator'>
-                                    <FloatingActionButton
+                                    <FloatingActionButton data-tip='Create Skill'
                                         backgroundColor={colors.fabButton} className='select'>
                                         <Add />
                                     </FloatingActionButton>
+                                    <ReactTooltip  effect='solid' place='bottom'/>
                                 </Link>
                             </div>
                         </div>
