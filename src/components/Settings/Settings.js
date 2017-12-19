64a4e5d113e8f291791cbaf1c6705c3f7936017d
Vivek Iyer <vivekbalasundaram@gmail.com>
2017-12-19 14:43:42 +0530

    Fixed travis error; Changed var back to let

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -59,8 +59,8 @@ class Settings extends React.Component {
         }
     }
     componentDidMount(){
-        var state = this.state;
-        var url = BASE_URL+'/aaa/listUserSettings.json?'
+        let state = this.state;
+        let url = BASE_URL+'/aaa/listUserSettings.json?'
             +'access_token='+cookies.get('loggedIn');
         // For Anonymous Users
         if(cookies.get('loggedIn')===null||
@@ -84,7 +84,7 @@ class Settings extends React.Component {
                     success: function (data) {
                         if(!data.settings){
                             debugger;
-                            state.theme = "light";
+                            state.theme = 'light';
                         }
                         else{
                             state.theme = data.settings.theme;
