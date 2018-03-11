ed62c528a957799995f341556a13af4f1b703c01
PRP <prp9812@gmail.com>
2018-03-11 10:40:51 +0530

    Add Footer

---
+++ src/components/Footer/Footer.react.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: false,
+    }
+  }
+  render() {
+    // Footer Component
+    return (
+      <div className='footer-wrapper'>
+
+        <div className='footer'>
+          <div className='footer-container'>
+            <ul className='alignLeft'>
+              <li><a href='https://chat.susi.ai/overview'>Overview</a></li>
+              <li><a href='https://chat.susi.ai/blog'>Blog</a></li>
+              <li><a href='https://github.com/fossasia?utf8=%E2%9C%93&q=susi'>Code</a></li>
+            </ul>
+            <ul className='alignRight'>
+              <li><a href='https://chat.susi.ai/privacy'>Privacy</a></li>
+              <li><a href='https://chat.susi.ai/terms'>Terms</a></li>
+              <li><a href='https://chat.susi.ai/contact'>Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
