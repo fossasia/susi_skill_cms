847c3f739ba01ecfd77bf5a6e71380b2b31c4e0d
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-05-31 14:31:30 +0530

    Create Menu Options for botbuilder

---
+++ src/components/BotBuilder/BotBuilderPages/Design.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Design extends Component {
+  render() {
+    return (
+        <div>
+          <h2>In Design Tab</h2>
+        </div>
+      );
+  }
+}
+
+export default Design;
