8477535d734e68ed6b86f28bf186fa13868dd898
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-06-01 00:09:36 +0530

    Fix bug with send button sending an empty string (#524)

--- public/susi-chatbot.js
+++ public/susi-chatbot.js
@@ -166,9 +166,11 @@ function enableBot(){
 		});
 		$('.susi-send-button').click(function(){
 			var text = $("#txMessage").val();
-			$("#chat-input").blur();
-			setUserResponse(text);
-			send(text);
+			if(text !== '') {
+				$("#chat-input").blur();
+				setUserResponse(text);
+				send(text);
+			}
 		});
 
 		// Send request to SUSI API
