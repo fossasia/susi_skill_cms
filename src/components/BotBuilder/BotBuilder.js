abe51292c804670feba3695ae92c0994155df9ed
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-06-01 12:22:55 +0530

    Update tabs as per material-ui v0.18

--- src/components/BotBuilder/BotBuilder.js
+++ src/components/BotBuilder/BotBuilder.js
@@ -38,72 +38,50 @@ class BotBuilder extends React.Component {
             textAlign: 'right',
             marginTop:'20px'
         };
-        const tabContent = {
-            textAlign: 'left',
-            padding: '20px 0px 0px 10px'
-        };
         return(
             <div>
                 <StaticAppBar {...this.props} />
                 <div style={styles.home}>
                     <Paper style={style} zDepth={1}>
-                        <Tabs
-                            value={this.state.activeTab}
-                            onChange={this.handleChange}
-                            indicatorColor="primary"
-                            textColor="primary"
-                            >
-                                <Tab label="Builder" onClick/>
-                                <Tab label="Design" />
-                                <Tab label="Analytics" />
-                            </Tabs>
-                            <RaisedButton
-                                label='Deploy'
-                                backgroundColor={colors.header}
-                                labelColor='#fff'
-                                onClick={this.handleClickGenerate}
-                                style={{ width: '100px' }}
-                            />
-                            <br/><br/>
-                            <div className={'code-wrap '+(this.state.showCode?'show':'hide')}>
-                                <div className="code-box">
-                                    <code>
-                                        &lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;https://skills.susi.ai/susi-chatbot.js&quot;&gt;&lt;/script&gt;
-                                    </code>
-                                </div>
-                                <h4>Paste the above code just above <i>&lt;/body&gt;</i>
-                                tag in your website</h4>
+                        <Tabs>
+                            <Tab label="Builder">
+                                <Builder />
+                            </Tab>
+                            <Tab label="Design">
+                                <Design />
+                            </Tab>
+                            <Tab label="Analytics">
+                                <Analytics />
+                            </Tab>
+                        </Tabs>
+                        <RaisedButton
+                            label='Deploy'
+                            backgroundColor={colors.header}
+                            labelColor='#fff'
+                            onClick={this.handleClickGenerate}
+                            style={{ width: '100px' }}
+                        />
+                        <br/><br/>
+                        <div className={'code-wrap '+(this.state.showCode?'show':'hide')}>
+                            <div className="code-box">
+                                <code>
+                                    &lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;https://skills.susi.ai/susi-chatbot.js&quot;&gt;&lt;/script&gt;
+                                </code>
                             </div>
-                            <br />
-                            <RaisedButton
-                                label='Preview'
-                                style={{ width: '100px' }}
-                            />
-                            {
-                                this.state.activeTab === 0 &&
-                                <div style={tabContent}>
-                                    <Builder />
-                                </div>
-                            }
-                            {
-                                this.state.activeTab === 1 &&
-                                <div style={tabContent}>
-                                    <Design />
-                                </div>
-                            }
-                            {
-                                this.state.activeTab === 2 &&
-                                <div style={tabContent}>
-                                    <Analytics />
-                                </div>
-                            }
-
-                        </Paper>
-                    </div>
+                            <h4>Paste the above code just above <i>&lt;/body&gt;</i>
+                            tag in your website</h4>
+                        </div>
+                        <br />
+                        <RaisedButton
+                            label='Preview'
+                            style={{ width: '100px' }}
+                        />
+                    </Paper>
                 </div>
-            )
-        }
+            </div>
+        )
     }
+}
 
     const styles = {
         home: {
