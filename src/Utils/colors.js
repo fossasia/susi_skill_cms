7a3cee53495e7de6e3a1804cb8c36711889759c2
Akshat-Jain <akjn11@gmail.com>
2018-06-04 14:57:40 +0530

    Removed Theme feature from Skill CMS

--- src/Utils/colors.js
+++ src/Utils/colors.js
@@ -1,20 +1,6 @@
-
-import Cookies from 'universal-cookie';
-const cookies = new Cookies();
-let colors;
-if(cookies.get('settings')!==undefined){
-	// console.log(cookies.get('settings').theme);
-	colors = {
-    header: cookies.get('settings').theme === 'dark'?'#19324c':'#4285f4',
-    fabButton : cookies.get('settings').theme === 'dark'?'#19324c':'#4285f4'
+let colors = {
+	header: '#4285f4',
+	fabButton : '#4285f4'
 };
-}
-else {
-	colors = {
-    header: '#4285f4',
-    fabButton : '#4285f4'
-	};
-}
-
 
 export default colors
