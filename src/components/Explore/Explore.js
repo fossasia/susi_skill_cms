396b1a84dd4fd91312006dd178fa25aff6d447ef
Akshat Garg <coolakshat24@gmail.com>
2018-06-08 07:57:52 +0530

    Add new route /explore for Metrics page (#505)

---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import StaticAppBar from '../StaticAppBar/StaticAppBar.react';
+import './Explore.css';
+
+class Explore extends React.Component {
+
+    render() {
+        return(
+          <div>
+            <StaticAppBar {...this.props} />
+          </div>
+       )
+    }
+}
+
+
+Explore.propTypes = {
+};
+
+export default Explore;
