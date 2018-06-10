7a9c555c6861ee25523b6b802839d9db9103f75a
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2018-06-10 14:05:25 +0530

    fix #639 - add auto copy in contact bot page (#640)

--- src/components/BotBuilder/ContactBot.js
+++ src/components/BotBuilder/ContactBot.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import StaticAppBar from '../StaticAppBar/StaticAppBar.react';
+import Snackbar from 'material-ui/Snackbar';
 import Cookies from 'universal-cookie';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { Paper } from 'material-ui';
 import './BotBuilder.css';
 
 const cookies = new Cookies();
 
 class ContactBot extends React.Component {
+	constructor(){
+		super();
+		this.state={
+			copied:false
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<StaticAppBar {...this.props} />
 				<div style={styles.home} className="botbuilder-page-wrapper">
 					<Paper style={styles.paperStyle} className="botBuilder-page-card" zDepth={1}>
-					<div style={{padding: '40px 40px 40px 40px', textAlign: 'center'}}>
-						<h1>The SUSI AI Contact Bot for your website is ready!</h1><br/>
-						<div className="code-box">
-							<code>
-								&lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;https://skills.susi.ai/susi-contactbot.js&quot;&gt;&lt;/script&gt;
-							</code>
+						<div style={{padding: '40px 40px 40px 40px', textAlign: 'center'}}>
+							<h1>The SUSI AI Contact bot for your website is ready!</h1><br/>
+							<div className="code-box">
+								<code>
+									&lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;https://skills.susi.ai/susi-contactbot.js&quot;&gt;&lt;/script&gt;
+								</code>
+								<CopyToClipboard text={'<script type=\'text/javascript\' id=\'susi-bot-script\' data-token=\''+cookies.get('loggedIn')+'\' src=\'https://skills.susi.ai/susi-contactbot.js\'></script>'}
+								onCopy={() => this.setState({copied: true})}>
+								<span className='copy-button'>copy</span>
+							</CopyToClipboard>
 						</div>
 							<h4>Paste the above code just above
 								<i> &lt;/body&gt; </i>
@@ -26,15 +39,21 @@ class ContactBot extends React.Component {
 					</div>
 					</Paper>
 				</div>
-		 	</div>
+				<Snackbar
+					open={this.state.copied}
+					message='Copied to clipboard!'
+					autoHideDuration={2000}
+					onRequestClose={()=>{this.setState({copied:false})}}
+				/>
+			</div>
 		);
 	}
 }
 
 const styles = {
 	home: {
-        width: '100%'
-    },
+		width: '100%'
+	},
 	paperStyle: {
 		width: '100%',
 		marginTop:'20px'
