8e2fff1c3b48c3c98d9c0d0f054ea627036885e5
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2018-06-11 18:58:44 +0530

    make deploy url dynamic

--- src/components/BotBuilder/BotBuilderPages/Deploy.js
+++ src/components/BotBuilder/BotBuilderPages/Deploy.js
@@ -3,6 +3,7 @@ import Snackbar from 'material-ui/Snackbar';
 import Cookies from 'universal-cookie';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 const cookies = new Cookies();
+const host = window.location.protocol + '//' + window.location.host;
 class Deploy extends Component {
   constructor(){
     super();
@@ -13,30 +14,30 @@ class Deploy extends Component {
   render() {
     return (
       <div className="menu-page">
-         <h2>Deploy your bot to your own website</h2>
-       <br/>
+        <h2>Deploy your bot to your own website</h2>
+        <br/>
         <div className='code-wrap show'>
           <div className="code-box">
             <code>
-              &lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;https://skills.susi.ai/susi-chatbot.js&quot;&gt;&lt;/script&gt;
+              &lt;script type=&quot;text/javascript&quot; id=&quot;susi-bot-script&quot; data-token=&quot;{cookies.get('loggedIn')}&quot; src=&quot;{host}/susi-chatbot.js&quot;&gt;&lt;/script&gt;
             </code>
-            <CopyToClipboard text={'<script type=\'text/javascript\' id=\'susi-bot-script\' data-token=\''+cookies.get('loggedIn')+'\' src=\'https://skills.susi.ai/susi-chatbot.js\'></script>'}
-              onCopy={() => this.setState({copied: true})}>
-              <span className='copy-button'>copy</span>
-            </CopyToClipboard>
-          </div>
-          <h4>Paste the above code just above <i>&lt;/body&gt;</i>
-        &nbsp;tag in your website.</h4>
+            <CopyToClipboard text={'<script type=\'text/javascript\' id=\'susi-bot-script\' data-token=\''+cookies.get('loggedIn')+'\' src=\''+host+'/susi-chatbot.js\'></script>'}
+            onCopy={() => this.setState({copied: true})}>
+            <span className='copy-button'>copy</span>
+          </CopyToClipboard>
         </div>
-        <Snackbar
-          open={this.state.copied}
-          message='Copied to clipboard!'
-          autoHideDuration={2000}
-          onRequestClose={()=>{this.setState({copied:false})}}
-        />
+        <h4>Paste the above code just above <i>&lt;/body&gt;</i>
+        &nbsp;tag in your website.</h4>
       </div>
-    );
-  }
+      <Snackbar
+        open={this.state.copied}
+        message='Copied to clipboard!'
+        autoHideDuration={2000}
+        onRequestClose={()=>{this.setState({copied:false})}}
+      />
+    </div>
+  );
+}
 }
 
 
