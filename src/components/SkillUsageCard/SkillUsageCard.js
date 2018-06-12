7472ad6e05b65d2f337d95f86a0ea06481efc5ee
Anshuman <anshu.av97@gmail.com>
2018-06-12 13:43:57 +0530

    Fixes #657: Display a message when no skill usage data is present

--- src/components/SkillUsageCard/SkillUsageCard.js
+++ src/components/SkillUsageCard/SkillUsageCard.js
@@ -4,23 +4,35 @@ import PropTypes from 'prop-types';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import { Paper } from 'material-ui';
 
+// Static assets
+import './SkillUsage.css';
+
 class SkillUsageCard extends Component {
 	render() {
+
+		const totalSkillUsage = this.props.skill_usage.reduce((totalCount, day) => {
+			return totalCount + day.count
+		}, 0)
+		console.log(totalSkillUsage);
 		return(
 			<Paper className="margin-b-md margin-t-md">
 				<h1 className='title'>
 					Skill Usage
 				</h1>
-				<div className="skill-usage-graph">
-					<LineChart width={600} height={300} data={this.props.skill_usage}
-							margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-						<XAxis dataKey="date" padding={{right: 20}} />
-						<YAxis/>
-						<Tooltip wrapperStyle={{height: '60px'}}/>
-						<Legend />
-						<Line name='Skill usage count' type="monotone" dataKey="count" stroke="#82ca9d" activeDot={{r: 8}}/>
-					</LineChart>
-				</div>
+				{
+					totalSkillUsage > 0 ?
+					(<div className="skill-usage-graph">
+						<LineChart width={600} height={300} data={this.props.skill_usage}
+								margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+							<XAxis dataKey="date" padding={{right: 20}} />
+							<YAxis/>
+							<Tooltip wrapperStyle={{height: '60px'}}/>
+							<Legend />
+							<Line name='Skill usage count' type="monotone" dataKey="count" stroke="#82ca9d" activeDot={{r: 8}}/>
+						</LineChart>
+					</div>):
+					(<div className="default-message">No usage data available, try this skill now!</div>)
+				}
 			</Paper>
 		)
 	}
