2a6c59c06621aaf10d8d716c6cff9f94781a1945
Akshat Jain <akjn11@gmail.com>
2018-06-14 21:43:06 +0530

    Made Skill Details page responsive (#709)

--- src/components/SkillUsageCard/SkillUsageCard.js
+++ src/components/SkillUsageCard/SkillUsageCard.js
@@ -8,40 +8,74 @@ import { Paper } from 'material-ui';
 import './SkillUsage.css';
 
 class SkillUsageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+  }
+
+  componentDidMount = () => {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  };
+
+  updateWindowDimensions = () => {
+    this.setState({
+      width: window.innerWidth * 0.8 > 800 ? 800 : window.innerWidth * 0.8,
+    });
+  };
+
   render() {
     const totalSkillUsage = this.props.skill_usage.reduce((totalCount, day) => {
       return totalCount + day.count;
     }, 0);
+
     return (
-      <Paper className="margin-b-md margin-t-md">
-        <h1 className="title">Skill Usage</h1>
-        {totalSkillUsage > 0 ? (
-          <div className="skill-usage-graph">
-            <LineChart
-              width={600}
-              height={300}
-              data={this.props.skill_usage}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-              <XAxis dataKey="date" padding={{ right: 20 }} />
-              <YAxis />
-              <Tooltip wrapperStyle={{ height: '60px' }} />
-              <Legend />
-              <Line
-                name="Skill usage count"
-                type="monotone"
-                dataKey="count"
-                stroke="#82ca9d"
-                activeDot={{ r: 8 }}
-              />
-            </LineChart>
-          </div>
-        ) : (
-          <div className="default-message">
-            No usage data available, try this skill now!
-          </div>
-        )}
-      </Paper>
+      <div>
+        <Paper className="margin-b-md margin-t-md">
+          <h1 className="title">Skill Usage</h1>
+          {totalSkillUsage > 0 ? (
+            <div className="usage-section">
+              <div className="skill-usage-graph">
+                <LineChart
+                  width={this.state.width}
+                  height={300}
+                  data={this.props.skill_usage}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <XAxis dataKey="date" padding={{ right: 20 }} />
+                  <YAxis />
+                  <Tooltip wrapperStyle={{ height: '60px' }} />
+                  <Legend />
+                  <Line
+                    name="Skill usage count"
+                    type="monotone"
+                    dataKey="count"
+                    stroke="#82ca9d"
+                    activeDot={{ r: 8 }}
+                  />
+                </LineChart>
+              </div>
+              <div className="total-hits">
+                <div className="large-text">{totalSkillUsage}</div>
+                Hits this week
+              </div>
+            </div>
+          ) : (
+            <div className="default-message">
+              No usage data available, try this skill now!
+            </div>
+          )}
+        </Paper>
+      </div>
     );
   }
 }
