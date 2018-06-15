2529c4a474c4799924bc7c2f2fb4484a855156b9
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-06-15 15:29:43 +0530

    Add icons for conversation view and change bot icon for tree view

--- src/components/BotBuilder/BotBuilderPages/BuildViews/ConversationView.js
+++ src/components/BotBuilder/BotBuilderPages/BuildViews/ConversationView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import colors from '../../../../Utils/colors';
 import { Paper } from 'material-ui';
 import RaisedButton from 'material-ui/RaisedButton';
+import Person from 'material-ui/svg-icons/social/person';
 import $ from 'jquery';
 import './ConversationView.css';
 
@@ -53,7 +54,20 @@ class ConversationView extends Component {
   render() {
     return (
       <div style={{ padding: '10px 10px 20px 10px' }}>
-        <Paper id="message-container" style={styles.paperStyle} zDepth={1} />
+        <Paper id="message-container" style={styles.paperStyle} zDepth={1}>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div className="user-text-box">Hello</div>
+            <Person style={{ height: '40px' }} />
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAIISURBVFhH7di/S5VRHMfxa0RG0A+kKCGkIkhHazEKatAGB0EhCIeQhgajbBBqqSmJgiASKxpqkNZwcBBdRHELyoZESpD8A5QITNT0/TG/8CXO/eG9Hp8LPR94Dc+X5+F8eTj3nPPczP+U43iHGxtXZZinWMMyKlUot7yEGpSDKpRDDuAORvAL1uAUXuEcEksH5mFNZfMBR7FjqcALhJrJ5gfOYEfyAKEm8lGTVYgazakVhBooxHtEzQBssFk046GreZNoQq+rraIOUXII/u31wPITVje3oOyHrz9ClLTAD/Qdl9Dtat5HnMcTV5MJREkn/EDFmkOU3EdoQHmDWpzapLm5gNC9qkdJA+5lEVo+WhG69y7SlEVOQyeUL5iJ4BuGcB27sKVcxW+EJnkMw9iHgnIWS7CHtWNk+0WWYhqL7rofBUXHI3uoCzq9jLvadrmIk1Cjuv4DLVV5YwdPv+LHalDxO9RtFXLF75uvVdhMzAZPwGp+fw9G3xJ2s37BlpgN1sBqj1XIlbRBp6gG98DWPz8fYjaol6KDrGraq/OmDc9xeOPqb8bw7wClugBLO56h6O/pPoQGKZY2Av8CSs4xfEZosK1Sczex7dmNejSW4DKOII1lL+yYH6KpkWi0VITmmRlEotH/LaHGjP7QTDQ6CX9CqDm5gsRTjbf4CjvOj+Ia0qTJnUxmHfGs+A6k/UOLAAAAAElFTkSuQmCC"
+              alt="bot icon"
+              style={styles.botIcon}
+            />
+            <div className="bot-text-box">Hi! I&apos;m SUSI.</div>
+          </div>
+        </Paper>
         <form onSubmit={this.handleSubmit} style={{ marginTop: '15px' }}>
           <label>
             {this.state.textType === 'user' ? (
@@ -111,8 +125,11 @@ const styles = {
     borderRadius: '4px',
     cursor: 'pointer',
   },
-  icon: {
-    verticleAlign: 'middle',
+  botIcon: {
+    height: '33px',
+    paddingTop: '9px',
+    width: '21px',
+    color: 'rgba(0, 0, 0, 0.87)',
   },
 };
 
