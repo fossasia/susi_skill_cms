721dd54d7430f9e5f1572dd5e09b3498ff425d6e
Anshuman Verma <anshu.av97@gmail.com>
2018-06-20 12:57:41 +0530

    Fixes #787: Display feedback timestamp (#797)
    * Fixes #787: Display feedback timestamp
    
    * format timestamp

--- src/components/SkillFeedbackCard/SkillFeedbackCard.js
+++ src/components/SkillFeedbackCard/SkillFeedbackCard.js
@@ -89,6 +89,44 @@ class SkillFeedbackCard extends Component {
     this.handleDeleteClose();
   };
 
+  parseDate = dtstr => {
+    if (dtstr) {
+      // replace anything but numbers by spaces
+      dtstr = dtstr.replace(/\D/g, ' ');
+      // trim any hanging white space
+      dtstr = dtstr.replace(/\s+$/, '');
+      // split on space
+      var dtcomps = dtstr.split(' ');
+      // not all ISO 8601 dates can convert, as is
+      // unless month and date specified, invalid
+      if (dtcomps.length < 3) {
+        return 'Invalid date';
+      }
+      // if time not provided, set to zero
+      if (dtcomps.length < 4) {
+        dtcomps[3] = 0;
+        dtcomps[4] = 0;
+        dtcomps[5] = 0;
+      }
+      // modify month between 1 based ISO 8601 and zero based Date
+      dtcomps[1]--;
+      const convdt = new Date(
+        Date.UTC(
+          dtcomps[0],
+          dtcomps[1],
+          dtcomps[2],
+          dtcomps[3],
+          dtcomps[4],
+          dtcomps[5],
+        ),
+      );
+      let time = convdt.toString();
+      time = time.split(' ').slice(1, 4);
+      time[1] = `${time[1]},`;
+      return time.join(' ');
+    }
+  };
+
   render() {
     const editActions = [
       <FlatButton
@@ -138,7 +176,14 @@ class SkillFeedbackCard extends Component {
               leftAvatar={
                 <CircleImage name={data.email.toUpperCase()} size="40" />
               }
-              primaryText={data.email}
+              primaryText={
+                <div>
+                  <div>{data.email}</div>
+                  <div className="feedback-timestamp">
+                    {this.parseDate(data.timestamp)}
+                  </div>
+                </div>
+              }
               rightIconButton={
                 <IconMenu iconButtonElement={iconButtonElement}>
                   <MenuItem
@@ -170,7 +215,14 @@ class SkillFeedbackCard extends Component {
             leftAvatar={
               <CircleImage name={data.email.toUpperCase()} size="40" />
             }
-            primaryText={data.email}
+            primaryText={
+              <div>
+                <div>{data.email}</div>
+                <div className="feedback-timestamp">
+                  {this.parseDate(data.timestamp)}
+                </div>
+              </div>
+            }
             secondaryText={<p>{data.feedback}</p>}
           />
         );
