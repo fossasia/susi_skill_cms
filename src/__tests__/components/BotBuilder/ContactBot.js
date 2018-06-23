f15acb1eb5b69bb1fa7c55237e09e4786582bda2
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-06-23 11:21:55 +0530

    Remove contact bot files

--- src/__tests__/components/BotBuilder/ContactBot.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ContactBot from '../../../components/BotBuilder/ContactBot';
-import { shallow } from 'enzyme';
-
-describe('<ContactBot />', () => {
-  it('render ContactBot without crashing', () => {
-    shallow(<ContactBot />);
-  });
-});
