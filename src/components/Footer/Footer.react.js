7205513fa8bfddf3168e10856a311f03dcb8fcec
Akshat Garg <coolakshat24@gmail.com>
2018-07-04 00:02:16 +0530

    Fixes #903: Add SUSI icon on footer (#910)

--- src/components/Footer/Footer.react.js
+++ src/components/Footer/Footer.react.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import susi from '../images/susi-logo.svg';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   constructor(props) {
@@ -13,26 +15,31 @@ class Footer extends Component {
     return (
       <div className="footer-wrapper">
         <div className="footer">
+          <div className="footer-left">
+            <Link to="/">
+              <img src={susi} alt="SUSI" className="susi-logo" />
+            </Link>
+            <ul>
+              <li>
+                <a href="https://chat.susi.ai/overview">Overview</a>
+              </li>
+              <li>
+                <a href="https://chat.susi.ai/blog">Blog</a>
+              </li>
+              <li>
+                <a href="https://github.com/fossasia/susi_skill_cms">Code</a>
+              </li>
+            </ul>
+          </div>
           <ul>
             <li>
-              <a href="https://chat.susi.ai/overview">Overview</a>
-            </li>
-            <li>
-              <a href="https://chat.susi.ai/blog">Blog</a>
-            </li>
-            <li>
-              <a href="https://github.com/fossasia/susi_skill_cms">Code</a>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <a href="https://chat.susi.ai/privacy">Privacy</a>
+              <a href="https://chat.susi.ai/contact">Contact</a>
             </li>
             <li>
               <a href="https://chat.susi.ai/terms">Terms</a>
             </li>
             <li>
-              <a href="https://chat.susi.ai/contact">Contact</a>
+              <a href="https://chat.susi.ai/privacy">Privacy</a>
             </li>
           </ul>
         </div>
