7ceaa369ab3fd016a8ede7a38820105e0d6be9df
Shubham Gupta <shubhamg@outlook.com>
2018-07-18 04:24:33 +0530

    Update SkillCardList.js

--- src/components/SkillCardList/SkillCardList.js
+++ src/components/SkillCardList/SkillCardList.js
@@ -5,6 +5,167 @@ import PropTypes from 'prop-types';
 import CircleImage from '../CircleImage/CircleImage';
 import styles from './SkillCardStyle';
 
+function createListCard(
+  el,
+  skillName,
+  skillUrl,
+  authorName,
+  description,
+  image,
+  language,
+  skill,
+  examples,
+  totalRating,
+  averageRating,
+) {
+  const mobileView = window.innerWidth < 430;
+  if (mobileView) {
+    return (
+      <div style={styles.skillCard} key={el}>
+        <div style={styles.imageContainerMobile}>
+          {image ? (
+            <div style={styles.imageMobile}>
+              <img alt={skillName} src={image} style={styles.imageMobile} />
+            </div>
+          ) : (
+            <CircleImage name={skillName} size="96" />
+          )}
+        </div>
+        <div style={styles.content}>
+          <div style={styles.header}>
+            <div style={styles.title}>
+              <Link
+                key={el}
+                to={{
+                  pathname: `/${skill.group}/${skill.skill_tag}/${language}`,
+                  state: {
+                    url: skillUrl,
+                    element: el,
+                    name: el,
+                    modelValue: skill.model,
+                    groupValue: skill.group,
+                    languageValue: language,
+                  },
+                }}
+              >
+                <span>{skillName}</span>
+              </Link>
+            </div>
+            <div style={styles.authorName}>
+              <span>{authorName}</span>
+            </div>
+            <div style={{ lineHeight: 1.35, fontSize: 16 }}>
+              <span>Skills for Alexa</span>
+            </div>
+            <div style={styles.rating}>
+              <Ratings
+                style={{ display: 'flex' }}
+                rating={averageRating || 0}
+                widgetRatedColors="#ffbb28"
+                widgetDimensions="20px"
+                widgetSpacings="0px"
+              >
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+              </Ratings>
+              <span style={styles.totalRating} title="Total ratings">
+                {totalRating || 0}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div style={styles.skillCard} key={el}>
+      <div style={styles.imageContainer}>
+        {image ? (
+          <div style={styles.image}>
+            <Link
+              key={el}
+              to={{
+                pathname: `/${skill.group}/${skill.skill_tag}/${
+                  this.props.languageValue
+                }`,
+              }}
+            >
+              <img alt={skill_name} src={image} style={styles.image} />
+            </Link>
+          </div>
+        ) : (
+          <CircleImage name={skillName} size="160" />
+        )}
+      </div>
+      <div style={styles.content}>
+        <div style={styles.header}>
+          <div style={styles.title}>
+            <Link
+              key={el}
+              to={{
+                pathname: `/${skill.group}/${skill.skill_tag}/${language}`,
+                state: {
+                  url: skillUrl,
+                  element: el,
+                  name: el,
+                  modelValue: skill.model,
+                  groupValue: skill.group,
+                  languageValue: language,
+                },
+              }}
+            >
+              <span>{skillName}</span>
+            </Link>
+          </div>
+          <div style={styles.authorName}>
+            <span>{authorName}</span>
+          </div>
+        </div>
+        <div style={styles.details}>
+          <div style={styles.exampleSection}>
+            {examples.map((eg, index) => {
+              return (
+                <div key={index} style={styles.example}>
+                  &quot;{eg}&quot;
+                </div>
+              );
+            })}
+          </div>
+          <div style={styles.textData}>
+            <div style={styles.row}>
+              <div style={styles.rating}>
+                <Ratings
+                  style={{ display: 'flex' }}
+                  rating={averageRating || 0}
+                  widgetRatedColors="#ffbb28"
+                  widgetDimensions="20px"
+                  widgetSpacings="0px"
+                >
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                  <Ratings.Widget />
+                </Ratings>
+                <span style={styles.totalRating} title="Total ratings">
+                  {totalRating || 0}
+                </span>
+              </div>
+            </div>
+            <div style={styles.row}>
+              <div style={styles.descriptionTitle}>Description</div>
+              <div style={styles.description}>{description}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 class SkillCardList extends Component {
   constructor(props) {
     super(props);
@@ -33,16 +194,17 @@ class SkillCardList extends Component {
     let cards = [];
     Object.keys(this.state.skills).forEach(el => {
       let skill = this.state.skills[el];
-      let skill_name = 'Name not available',
+      let skillName = 'Name not available',
         examples = [],
         image = '',
         description = 'No description available',
-        author_name = 'Author',
-        average_rating = 0,
-        total_rating = 0;
+        authorName = 'Author',
+        skillUrl = this.props.skillUrl,
+        averageRating = 0,
+        totalRating = 0;
       if (skill.skill_name) {
-        skill_name = skill.skill_name;
-        skill_name = skill_name.charAt(0).toUpperCase() + skill_name.slice(1);
+        skillName = skill.skill_name;
+        skillName = skillName.charAt(0).toUpperCase() + skillName.slice(1);
       }
       if (skill.image) {
         image = `https://raw.githubusercontent.com/fossasia/susi_skill_data/master/models/${
@@ -57,97 +219,28 @@ class SkillCardList extends Component {
         description = skill.descriptions;
       }
       if (skill.author) {
-        author_name = skill.author;
+        authorName = skill.author;
       }
       if (skill.skill_rating) {
-        average_rating = parseFloat(skill.skill_rating.stars.avg_star);
-        total_rating = parseInt(skill.skill_rating.stars.total_star, 10);
+        averageRating = parseFloat(skill.skill_rating.stars.avg_star);
+        totalRating = parseInt(skill.skill_rating.stars.total_star, 10);
       }
+
+      let language = this.props.languageValue;
       cards.push(
-        <div style={styles.skillCard} key={el}>
-          <div style={styles.imageContainer}>
-            {image ? (
-              <div style={styles.image}>
-                <Link
-                  key={el}
-                  to={{
-                    pathname: `/${skill.group}/${skill.skill_tag}/${
-                      this.props.languageValue
-                    }`,
-                  }}
-                >
-                  <img alt={skill_name} src={image} style={styles.image} />
-                </Link>
-              </div>
-            ) : (
-              <CircleImage name={el} size="218" />
-            )}
-          </div>
-          <div style={styles.content}>
-            <div style={styles.header}>
-              <Link
-                key={el}
-                to={{
-                  pathname: `/${skill.group}/${skill.skill_tag}/${
-                    this.props.languageValue
-                  }`,
-                  state: {
-                    url: this.props.skillUrl,
-                    element: el,
-                    name: el,
-                    modelValue: this.props.modelValue,
-                    groupValue: skill.group,
-                    languageValue: this.props.languageValue,
-                  },
-                }}
-              >
-                <div style={styles.title}>
-                  <span>{skill_name}</span>
-                </div>
-              </Link>
-              <div style={styles.authorName}>
-                <span>{author_name}</span>
-              </div>
-            </div>
-            <div style={styles.details}>
-              <div style={styles.exampleSection}>
-                {examples.map((eg, index) => {
-                  return (
-                    <div key={index} style={styles.example}>
-                      &quot;{eg}&quot;
-                    </div>
-                  );
-                })}
-              </div>
-              <div style={styles.textData}>
-                <div style={styles.row}>
-                  <div style={styles.rating}>
-                    <Ratings
-                      style={{ display: 'flex' }}
-                      rating={average_rating || 0}
-                      widgetRatedColors="#ffbb28"
-                      widgetDimensions="20px"
-                      widgetSpacings="0px"
-                    >
-                      <Ratings.Widget />
-                      <Ratings.Widget />
-                      <Ratings.Widget />
-                      <Ratings.Widget />
-                      <Ratings.Widget />
-                    </Ratings>
-                    <span style={styles.totalRating} title="Total ratings">
-                      {total_rating || 0}
-                    </span>
-                  </div>
-                </div>
-                <div style={styles.row}>
-                  <div style={styles.descriptionTitle}>Description</div>
-                  <div style={styles.description}>{description}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>,
+        createListCard(
+          el,
+          skillName,
+          skillUrl,
+          authorName,
+          description,
+          image,
+          language,
+          skill,
+          examples,
+          totalRating,
+          averageRating,
+        ),
       );
     });
     this.setState({
