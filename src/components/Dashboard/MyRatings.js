92178df5a44f5319291e564d1f5ec7d3d5a859e7
Anshuman Verma <anshu.av97@gmail.com>
2018-07-18 20:32:49 +0530

    Fixes #1173: Remove extra table row in dashboard (#1174)

--- src/components/Dashboard/MyRatings.js
+++ src/components/Dashboard/MyRatings.js
@@ -111,7 +111,6 @@ class MyRatings extends Component {
                     </TableRow>
                   );
                 })}
-                <TableRow />
               </TableBody>
             </Table>
           </div>
