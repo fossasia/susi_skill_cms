74aa801fb6f4b7ea59f0149b09b83a975d0964f9
Anshuman <anshu.av97@gmail.com>
2018-07-21 21:46:51 +0530

    Updates as per server PR

--- src/components/BrowseSkill/BrowseSkill.js
+++ src/components/BrowseSkill/BrowseSkill.js
@@ -347,7 +347,7 @@ export default class BrowseSkill extends React.Component {
           topUsedSkills: data.metrics.usage,
           latestSkills: data.metrics.latest,
           topFeedbackSkills: data.metrics.feedback,
-          topGames: data.metrics.games,
+          topGames: data.metrics.topGames,
         });
       },
       error: function(e) {
