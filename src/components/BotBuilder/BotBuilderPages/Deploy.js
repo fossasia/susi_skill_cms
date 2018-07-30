bed56e117a37e15183b7e46d6593a4b81e6575cc
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2018-07-30 23:09:18 +0530

    show instructions in deploy page step wise (#1365)

--- src/components/BotBuilder/BotBuilderPages/Deploy.js
+++ src/components/BotBuilder/BotBuilderPages/Deploy.js
@@ -14,47 +14,58 @@ class Deploy extends Component {
   }
   render() {
     let { group, language, skill } = this.props;
+    let part = ` data-skill=&quot;${skill}&quot; src=&quot;${api}/susi-chatbot.js&quot;`;
+    let code = `
+    &lt;script type=&quot;text/javascript&quot;
+    id=&quot;susi-bot-script&quot; data-userid=&quot;${cookies.get(
+      'uuid',
+    )}&quot; data-group=&quot;${group}&quot; data-language=&quot;${language}&quot;${part}
+    &gt;&lt;/script&gt;
+    `;
     return (
       <div className="menu-page">
         <h1 style={{ lineHeight: '50px' }}>
           4. Deploy your bot to your own website
         </h1>
         <br />
-        <div className="code-wrap show">
-          <div className="code-box">
-            <code>
-              &lt;script type=&quot;text/javascript&quot;
-              id=&quot;susi-bot-script&quot; data-userid=&quot;{cookies.get(
-                'uuid',
-              )}&quot; data-group=&quot;{group}&quot; data-language=&quot;{
-                language
-              }&quot; data-skill=&quot;{skill}&quot; src=&quot;{api}/susi-chatbot.js&quot;
-              &gt; &lt;/script&gt;
-            </code>
-            <CopyToClipboard
-              text={
-                "<script type='text/javascript' id='susi-bot-script' data-userid='" +
-                cookies.get('uuid') +
-                "' data-group='" +
-                group +
-                "' data-language='" +
-                language +
-                "' data-skill='" +
-                skill +
-                "' src='" +
-                api +
-                "/susi-chatbot.js'></script>"
-              }
-              onCopy={() => this.setState({ copied: true })}
-            >
-              <span className="copy-button">copy</span>
-            </CopyToClipboard>
-          </div>
-          <h4>
-            Paste the above code just above <i>&lt;/body&gt;</i>
-            &nbsp;tag in your website.
-          </h4>
-        </div>
+        <ol className="deploy-steps">
+          <li>
+            Add the code below to every page you want the Messenger to appear.
+            Copy and paste it before the{' '}
+            <span className="html-tag">&lt;body&gt;</span> tag on each page.
+            <br />
+            <br />
+            <div className="code-wrap show">
+              <div className="code-box">
+                <code dangerouslySetInnerHTML={{ __html: code }} />
+                <CopyToClipboard
+                  text={
+                    "<script type='text/javascript' id='susi-bot-script' data-userid='" +
+                    cookies.get('uuid') +
+                    "' data-group='" +
+                    group +
+                    "' data-language='" +
+                    language +
+                    "' data-skill='" +
+                    skill +
+                    "' src='" +
+                    api +
+                    "/susi-chatbot.js'></script>"
+                  }
+                  onCopy={() => this.setState({ copied: true })}
+                >
+                  <span className="copy-button">copy</span>
+                </CopyToClipboard>
+              </div>
+            </div>
+          </li>
+          <li>
+            Open your web app or website and look for the Messenger in the
+            bottom right corner.
+          </li>
+          <li>Start chatting with your SUSI bot.</li>
+        </ol>
+
         <Snackbar
           open={this.state.copied}
           message="Copied to clipboard!"
