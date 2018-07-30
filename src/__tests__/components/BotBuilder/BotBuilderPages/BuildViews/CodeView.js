11019676a4d10eaed2dc73e94229ca381e71381a
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-07-30 18:09:28 +0530

    Make category, language and skill name options appear on all three views

--- src/__tests__/components/BotBuilder/BotBuilderPages/BuildViews/CodeView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CodeView from '../../../../../components/BotBuilder/BotBuilderPages/BuildViews/CodeView';
-import { shallow } from 'enzyme';
-
-describe('<CodeView />', () => {
-  it('render CodeView without crashing', () => {
-    shallow(<CodeView />);
-  });
-});
--- src/__tests__/components/BotBuilder/BotBuilderPages/BuildViews/TreeView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TreeView from '../../../../../components/BotBuilder/BotBuilderPages/BuildViews/TreeView';
-import { shallow } from 'enzyme';
-
-describe('<TreeView />', () => {
-  it('render TreeView without crashing', () => {
-    shallow(<TreeView />);
-  });
-});
