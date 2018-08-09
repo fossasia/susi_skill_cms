b047395138a1d7e38e740a91294af9e6a4a55298
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-08-09 06:29:34 +0530

    Fetch data for conversation view from server (#1490)

--- src/components/SkillCreator/SkillViews/ConversationView.js
+++ src/components/SkillCreator/SkillViews/ConversationView.js
@@ -1,81 +1,80 @@
 import React, { Component } from 'react';
 import { Paper } from 'material-ui';
 import PropTypes from 'prop-types';
-import RaisedButton from 'material-ui/RaisedButton';
 import Snackbar from 'material-ui/Snackbar';
 import Person from 'material-ui/svg-icons/social/person';
-import Delete from 'material-ui/svg-icons/action/delete';
-import { colors } from '../../../utils';
+import CircularProgress from 'material-ui/CircularProgress';
+import { urls } from '../../../utils';
 import './ConversationView.css';
+import $ from 'jquery';
 
 class ConversationView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      textType: '',
-      textBoxValue: '',
       openSnackbar: false,
       msgSnackbar: '',
+      code: this.props.skillCode,
+      conversationsData: [],
+      loaded: false,
     };
   }
 
   componentDidMount = () => {
+    this.getResponses(0);
+  };
+
+  getResponses = responseNumber => {
     let skillData = this.props.skillData;
-    let noOfConv = skillData.children.length;
-    if (skillData.children[noOfConv - 1].children.length === 0) {
-      this.setState({ textType: 'bot' });
-    } else {
-      this.setState({ textType: 'user' });
+    let i = skillData.children[responseNumber];
+    let conversationsData = this.state.conversationsData;
+    if (i) {
+      let url = urls.API_URL + '/susi/chat.json?q=';
+      conversationsData.push({
+        type: 'user',
+        name: i.name,
+        id: 'u' + responseNumber,
+      });
+      url += `${encodeURIComponent(i.name)}&instant=${encodeURIComponent(
+        this.state.code,
+      )}`;
+      $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(data) {
+          let answer;
+          if (data.answers[0]) {
+            answer = data.answers[0].actions[0].expression;
+          } else {
+            answer = 'Sorry, I could not understand what you just said.';
+          }
+          conversationsData.push({
+            type: 'bot',
+            name: answer,
+            id: 'b' + responseNumber,
+          });
+          if (responseNumber + 1 === skillData.children.length) {
+            this.setState({ loaded: true });
+          }
+          this.setState({ conversationsData }, () =>
+            this.getResponses(++responseNumber),
+          );
+        }.bind(this),
+        error: function(err) {
+          console.log(err);
+          this.setState({
+            openSnackbar: true,
+            msgSnackbar: 'Unable to load conversation view. Please try again.',
+          });
+        }.bind(this),
+      });
     }
-  };
-
-  handleChange = event => {
-    this.setState({
-      value: event.target.value,
-      textBoxValue: event.target.value,
-    });
-  };
-
-  handleSubmit = event => {
-    if (this.state.value !== '') {
-      this.props.handleAddNode(this.state.value);
-      if (this.state.textType === 'bot') {
-        this.setState({ textType: 'user' });
-      } else if (this.state.textType === 'user') {
-        this.setState({ textType: 'bot' });
-      }
-      this.setState({ textBoxValue: '', value: '' });
-      event.preventDefault();
-    }
-  };
-
-  handleDeleteNode = node => {
-    this.props.handleDeleteNode(node);
   };
 
   render() {
-    let skillData = this.props.skillData;
-    let conversationsData = [];
-    if (skillData) {
-      for (let i of skillData.children) {
-        conversationsData.push({
-          type: 'user',
-          name: i.name,
-          id: i.id,
-        });
-
-        if (i.children) {
-          for (let j of i.children) {
-            conversationsData.push({
-              type: 'bot',
-              name: j.name,
-              id: j.id,
-            });
-          }
-        }
-      }
-    }
+    let conversationsData = this.state.conversationsData;
     return (
       <div
         style={{
@@ -83,77 +82,48 @@ class ConversationView extends Component {
         }}
       >
         <Paper id="message-container" style={styles.paperStyle} zDepth={1}>
-          {conversationsData.map(item => {
-            let text = item.name;
-            let messageArr = text.match(/.{1,28}/g);
-            let message = [];
-            if (messageArr && messageArr.length > 0) {
-              for (let i = 0; i < messageArr.length; i++) {
-                message.push(<div>{messageArr[i]}</div>);
+          {!this.state.loaded ? (
+            <div className="center">
+              <CircularProgress size={62} color="#4285f5" />
+              <h4>Loading</h4>
+            </div>
+          ) : (
+            conversationsData.map(item => {
+              let text = item.name;
+              let messageArr = text.match(/.{1,28}/g);
+              let message = [];
+              if (messageArr && messageArr.length > 0) {
+                for (let i = 0; i < messageArr.length; i++) {
+                  message.push(<div>{messageArr[i]}</div>);
+                }
               }
-            }
-            if (item.type === 'user') {
+              if (item.type === 'user') {
+                return (
+                  <div
+                    key={item.id}
+                    style={{ display: 'flex', flexDirection: 'row' }}
+                  >
+                    <div className="user-text-box">{message}</div>
+                    <Person style={{ height: '40px' }} />
+                  </div>
+                );
+              }
               return (
                 <div
                   key={item.id}
                   style={{ display: 'flex', flexDirection: 'row' }}
                 >
-                  <Delete
-                    style={styles.deleteUser}
-                    onClick={() => this.handleDeleteNode(item)}
+                  <img
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAIISURBVFhH7di/S5VRHMfxa0RG0A+kKCGkIkhHazEKatAGB0EhCIeQhgajbBBqqSmJgiASKxpqkNZwcBBdRHELyoZESpD8A5QITNT0/TG/8CXO/eG9Hp8LPR94Dc+X5+F8eTj3nPPczP+U43iHGxtXZZinWMMyKlUot7yEGpSDKpRDDuAORvAL1uAUXuEcEksH5mFNZfMBR7FjqcALhJrJ5gfOYEfyAKEm8lGTVYgazakVhBooxHtEzQBssFk046GreZNoQq+rraIOUXII/u31wPITVje3oOyHrz9ClLTAD/Qdl9Dtat5HnMcTV5MJREkn/EDFmkOU3EdoQHmDWpzapLm5gNC9qkdJA+5lEVo+WhG69y7SlEVOQyeUL5iJ4BuGcB27sKVcxW+EJnkMw9iHgnIWS7CHtWNk+0WWYhqL7rofBUXHI3uoCzq9jLvadrmIk1Cjuv4DLVV5YwdPv+LHalDxO9RtFXLF75uvVdhMzAZPwGp+fw9G3xJ2s37BlpgN1sBqj1XIlbRBp6gG98DWPz8fYjaol6KDrGraq/OmDc9xeOPqb8bw7wClugBLO56h6O/pPoQGKZY2Av8CSs4xfEZosK1Sczex7dmNejSW4DKOII1lL+yYH6KpkWi0VITmmRlEotH/LaHGjP7QTDQ6CX9CqDm5gsRTjbf4CjvOj+Ia0qTJnUxmHfGs+A6k/UOLAAAAAElFTkSuQmCC"
+                    alt="bot icon"
+                    style={styles.botIcon}
                   />
-                  <div className="user-text-box">{message}</div>
-                  <Person style={{ height: '40px' }} />
+                  <div className="bot-text-box">{message}</div>
                 </div>
               );
-            }
-            return (
-              <div
-                key={item.id}
-                style={{ display: 'flex', flexDirection: 'row' }}
-              >
-                <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAIISURBVFhH7di/S5VRHMfxa0RG0A+kKCGkIkhHazEKatAGB0EhCIeQhgajbBBqqSmJgiASKxpqkNZwcBBdRHELyoZESpD8A5QITNT0/TG/8CXO/eG9Hp8LPR94Dc+X5+F8eTj3nPPczP+U43iHGxtXZZinWMMyKlUot7yEGpSDKpRDDuAORvAL1uAUXuEcEksH5mFNZfMBR7FjqcALhJrJ5gfOYEfyAKEm8lGTVYgazakVhBooxHtEzQBssFk046GreZNoQq+rraIOUXII/u31wPITVje3oOyHrz9ClLTAD/Qdl9Dtat5HnMcTV5MJREkn/EDFmkOU3EdoQHmDWpzapLm5gNC9qkdJA+5lEVo+WhG69y7SlEVOQyeUL5iJ4BuGcB27sKVcxW+EJnkMw9iHgnIWS7CHtWNk+0WWYhqL7rofBUXHI3uoCzq9jLvadrmIk1Cjuv4DLVV5YwdPv+LHalDxO9RtFXLF75uvVdhMzAZPwGp+fw9G3xJ2s37BlpgN1sBqj1XIlbRBp6gG98DWPz8fYjaol6KDrGraq/OmDc9xeOPqb8bw7wClugBLO56h6O/pPoQGKZY2Av8CSs4xfEZosK1Sczex7dmNejSW4DKOII1lL+yYH6KpkWi0VITmmRlEotH/LaHGjP7QTDQ6CX9CqDm5gsRTjbf4CjvOj+Ia0qTJnUxmHfGs+A6k/UOLAAAAAElFTkSuQmCC"
-                  alt="bot icon"
-                  style={styles.botIcon}
-                />
-                <div className="bot-text-box">{message}</div>
-                <Delete
-                  style={styles.deleteBot}
-                  onClick={() => this.handleDeleteNode(item)}
-                />
-              </div>
-            );
-          })}
+            })
+          )}
         </Paper>
-        <form onSubmit={this.handleSubmit} style={{ marginTop: '15px' }}>
-          <label>
-            {this.state.textType === 'user' ? (
-              <div style={{ fontSize: '14px' }}>Type the user message:</div>
-            ) : (
-              <div style={{ fontSize: '14px' }}>Type the bot response:</div>
-            )}
-            <input
-              id="text-box"
-              type="text"
-              placeholder={
-                this.state.textType === 'user'
-                  ? 'Enter the user message'
-                  : 'Enter the bot response'
-              }
-              value={this.state.textBoxValue}
-              onChange={this.handleChange}
-              style={styles.textBox}
-            />
-          </label>
-          <RaisedButton
-            label="Submit"
-            backgroundColor={colors.header}
-            labelColor="#fff"
-            onTouchTap={this.handleSubmit}
-            style={{ marginTop: 10 }}
-          />
-        </form>
         <Snackbar
           open={this.state.openSnackbar}
           message={this.state.msgSnackbar}
@@ -168,29 +138,9 @@ class ConversationView extends Component {
 }
 
 const styles = {
-  textBox: {
-    width: '100%',
-    fontSize: '14px',
-    padding: '12px 20px',
-    margin: '8px 0',
-    display: 'inline-block',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    boxSizing: 'border-box',
-  },
   paperStyle: {
     width: '100%',
     padding: '20px 20px 20px 20px',
-  },
-  submitButton: {
-    width: '40px',
-    backgroundColor: 'rgb(66, 133, 244)',
-    color: 'white',
-    padding: '14px 20px',
-    margin: '8px 0',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
   },
   botIcon: {
     height: '33px',
@@ -198,27 +148,9 @@ const styles = {
     width: '21px',
     color: 'rgba(0, 0, 0, 0.87)',
   },
-  deleteUser: {
-    height: '33px',
-    marginLeft: 'auto',
-    paddingTop: '10px',
-    paddingRight: '10px',
-    width: '32px',
-    cursor: 'pointer',
-  },
-  deleteBot: {
-    height: '31px',
-    marginRight: 'auto',
-    paddingTop: '8px',
-    paddingLeft: '10px',
-    width: '32px',
-    cursor: 'pointer',
-  },
 };
 ConversationView.propTypes = {
+  skillCode: PropTypes.string,
   skillData: PropTypes.object,
-  handleDeleteNode: PropTypes.func,
-  handleAddNode: PropTypes.func,
-  botbuilder: PropTypes.bool,
 };
 export default ConversationView;
