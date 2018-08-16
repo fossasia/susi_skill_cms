84592e6ea7eb381723c8d1b4f5ac02c9016b271a
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2018-08-17 00:01:36 +0530

    improve deploy section

--- src/components/BotBuilder/BotBuilderPages/Deploy.js
+++ src/components/BotBuilder/BotBuilderPages/Deploy.js
@@ -27,44 +27,48 @@ class Deploy extends Component {
         <h1 style={{ lineHeight: '50px' }}>
           4. Deploy your bot to your own website
         </h1>
-        <br />
-        <ol className="deploy-steps">
-          <li>
-            Add the code below to every page you want the Messenger to appear.
-            Copy and paste it before the{' '}
-            <span className="html-tag">&lt;body&gt;</span> tag on each page.
-            <br />
-            <br />
-            <div className="code-wrap show">
-              <div className="code-box">
-                <code dangerouslySetInnerHTML={{ __html: code }} />
-                <CopyToClipboard
-                  text={
-                    "<script type='text/javascript' id='susi-bot-script' data-userid='" +
-                    cookies.get('uuid') +
-                    "' data-group='" +
-                    group +
-                    "' data-language='" +
-                    language +
-                    "' data-skill='" +
-                    skill +
-                    "' src='" +
-                    api +
-                    "/susi-chatbot.js'></script>"
-                  }
-                  onCopy={() => this.setState({ copied: true })}
-                >
-                  <span className="copy-button">copy</span>
-                </CopyToClipboard>
-              </div>
-            </div>
-          </li>
-          <li>
-            Open your web app or website and look for the Messenger in the
-            bottom right corner.
-          </li>
-          <li>Start chatting with your SUSI bot.</li>
-        </ol>
+        <div style={{ marginLeft: 'auto', marginRight: '0px' }}>
+          <div style={{ padding: '30px 10px 0 10px' }}>
+            <ol className="deploy-steps">
+              <li>
+                Add the code below to every page you want the Messenger to
+                appear. Copy and paste it before the{' '}
+                <span className="html-tag">&lt;&#47;body&gt;</span> tag on each
+                page.
+                <br />
+                <br />
+                <div className="code-wrap show">
+                  <div className="code-box">
+                    <code dangerouslySetInnerHTML={{ __html: code }} />
+                    <CopyToClipboard
+                      text={
+                        "<script type='text/javascript' id='susi-bot-script' data-userid='" +
+                        cookies.get('uuid') +
+                        "' data-group='" +
+                        group +
+                        "' data-language='" +
+                        language +
+                        "' data-skill='" +
+                        skill +
+                        "' src='" +
+                        api +
+                        "/susi-chatbot.js'></script>"
+                      }
+                      onCopy={() => this.setState({ copied: true })}
+                    >
+                      <span className="copy-button">copy</span>
+                    </CopyToClipboard>
+                  </div>
+                </div>
+              </li>
+              <li>
+                Open your web app or website and look for the Messenger in the
+                bottom right corner.
+              </li>
+              <li>Start chatting with your SUSI bot.</li>
+            </ol>
+          </div>
+        </div>
 
         <Snackbar
           open={this.state.copied}
