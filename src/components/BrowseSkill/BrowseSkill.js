93a34678bf199b157e853bb42d6b9f100f1f55e9
Samagra Gupta <32234926+samagragupta@users.noreply.github.com>
2018-12-08 15:15:17 +0530

    Backward button is working (#1715)
    * Backward button is working
    
    * used destructuring

--- src/components/BrowseSkill/BrowseSkill.js
+++ src/components/BrowseSkill/BrowseSkill.js
@@ -183,14 +183,13 @@ export default class BrowseSkill extends React.Component {
 
   handleNavigationBackward = () => {
     $('html, body').animate({ scrollTop: 0 }, 'fast');
-    const { listPage, listOffset, entriesPerPage, skills } = this.state;
-    const newListPage = listPage - 1;
-    const newListOffset = listOffset - entriesPerPage;
-    this.setState({
-      listPage: newListPage,
-      listOffset: newListOffset,
-      listSkills: skills.slice(listOffset, listOffset),
-    });
+    const { listOffset, entriesPerPage, skills } = this.state;
+    let newlistOffset = listOffset - entriesPerPage;
+    this.setState(prevState => ({
+      listPage: prevState.listPage - 1,
+      listOffset: prevState.listOffset - prevState.entriesPerPage,
+      listSkills: skills.slice(newlistOffset, prevState.listOffset),
+    }));
   };
 
   handleShowSkills = ({ reviewed, staffPicks }) => {
