2dee8f4b0cacf9da85094d5d2b059618ce928403
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-12-27 12:24:52 +0530

    Fixes #1712: Signup.react.js connected to redux store and refactored

--- src/redux/actionTypes/index.js
+++ src/redux/actionTypes/index.js
@@ -1,3 +1,7 @@
 import _ from 'lodash';
 
-export default _.mapKeys(['APP_GET_API_KEYS', 'APP_GET_LOGIN']);
+export default _.mapKeys([
+  'APP_GET_API_KEYS',
+  'APP_GET_LOGIN',
+  'APP_GET_SIGNUP',
+]);
