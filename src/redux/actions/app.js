655098bf36fc3698d0dc03bfbf916ef9787f5346
Shubham Gupta <shubhamg@outlook.com>
2019-01-16 19:24:36 +0530

    Hydrate store with cookies#1830, Add logout redux files#1831

--- src/redux/actions/app.js
+++ src/redux/actions/app.js
@@ -2,12 +2,13 @@ import { createAction } from 'redux-actions';
 import actionTypes from '../actionTypes';
 import { getLogin, getSignup, fetchApiKeys } from '../../api';
 
-// const returnArgumentsFn = function(payload) {
-//   return Promise.resolve(payload);
-// };
+const returnArgumentsFn = function(payload) {
+  return Promise.resolve(payload);
+};
 
 export default {
   getApiKeys: createAction(actionTypes.APP_GET_API_KEYS, fetchApiKeys),
   getLogin: createAction(actionTypes.APP_GET_LOGIN, getLogin),
   getSignup: createAction(actionTypes.APP_GET_SIGNUP, getSignup),
+  logout: createAction(actionTypes.APP_LOGOUT, returnArgumentsFn),
 };
