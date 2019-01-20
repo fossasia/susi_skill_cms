e0583f960c9b6e82a3713cd06e3babdb9f959116
Shubham Gupta <shubhamg@outlook.com>
2019-01-20 13:10:41 +0530

    Add snackbar#1808, Dialog UI#1815 reducer, action, action creator

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,12 @@ import { routerReducer } from 'react-router-redux';
 import app from './app';
 import skills from './skills';
 import skill from './skill';
+import ui from './ui';
+
 export default combineReducers({
   routing: routerReducer,
   app,
   skills,
   skill,
+  ui,
 });
