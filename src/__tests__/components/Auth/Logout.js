9e0164c08196158a1e805af1e488d7d793226799
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-01-20 17:27:41 +0530

    Fixes #1735: Force reload removed on logout

--- src/__tests__/components/Auth/Logout.js
+++ src/__tests__/components/Auth/Logout.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Logout from '../../../components/Auth/Logout.react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<Logout />', () => {
   it('render Logout without crashing', () => {
-    shallow(<Logout />);
+    shallow(
+      <Provider store={store}>
+        <Logout />
+      </Provider>,
+    );
   });
 });
