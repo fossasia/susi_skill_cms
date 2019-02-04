6051d2b8e71851fbe1d327c94c5934d9a0c46d89
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-02-05 01:25:52 +0530

    Fixes #1856: Workaround for failed captcha key loading implemented

--- src/components/Auth/SignUp/SignUp.js
+++ src/components/Auth/SignUp/SignUp.js
@@ -297,7 +297,7 @@ class SignUp extends Component {
       !passwordErrorMessage &&
       confirmPassword &&
       !passwordConfirmErrorMessage &&
-      isCaptchaVerified;
+      (isCaptchaVerified || !captchaKey);
 
     return (
       <Dialog
